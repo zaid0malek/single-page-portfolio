a69ba75a4409202e8b11323098e85c1ea3bd421f
zaid0malek <zaid.m@simformsolutions.com>
2023-02-02 17:42:02 +0530

    changed color palette

--- index.js
+++ index.js
@@ -10,15 +10,3 @@ navLinks.forEach(link => {
 
 
 
-//navbar hide on scroll down
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
-    }
-    prevScrollpos = currentScrollPos;
-}
-
